fa7bbcf55d18c80e160e825d3b8eb918bfffec49
Hung <trannguyenquochung00@gmail.com>
2024-02-28 10:51:24 +0700

    initial commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Hưng Trần on 26/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
